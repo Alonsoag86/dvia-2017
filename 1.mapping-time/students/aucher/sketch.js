5be68032481a11771e3548ee1237fb58bca4e013
serrau <aucher.serr@gmail.com>
2017-09-10 21:30:10 -0400

    add second circle structure

--- 1.mapping-time/students/aucher/sketch.js
+++ 1.mapping-time/students/aucher/sketch.js
@@ -6,9 +6,11 @@ var w = window.innerWidth,
 var cx, cy; // center position of canvas
 
 // Radius for hands of the clock
-var hourCircleRadius;
+var hourCirclePos;
+var minCirclePos;
 var clockDiameter;
 var hourDiameter;
+var minDiameter;
 
 function setup() {
   createCanvas(w, h);
@@ -16,8 +18,9 @@ function setup() {
 
   var radius = min(width, height) / 2; // this is the maximum possible radius
   clockDiameter = radius * 1.7; // make slightly smaller than maximum allowed
-  hourCircleRadius = clockDiameter / 2;
+  hourCirclePos = clockDiameter / 2;
   hourDiameter = (clockDiameter * TWO_PI) / (24 * 2); // largest diameter for 24
+  minDiameter = (hourDiameter * TWO_PI) / (60 * 2);
 
   cx = width / 2; // centers the clock
   cy = height / 2;
@@ -40,17 +43,25 @@ function draw() {
   // Draw the hands of the clock
   stroke(255);
   strokeWeight(1);
-  // line(cx, cy, cx + cos(s) * hourCircleRadius, cy + sin(s) * hourCircleRadius);
+  // line(cx, cy, cx + cos(s) * hourCirclePos, cy + sin(s) * hourCirclePos);
 
   // Draw the minute ticks
-  strokeWeight(3);
-  stroke(80);
-  beginShape(POINTS);
-  for (var a = 0; a < 360; a+=15) {
+    beginShape(POINTS);
+  for (var a = 0; a < 360; a+= 360/24) {
     var angle = radians(a);
-    var x = cx + cos(angle) * hourCircleRadius;
-    var y = cy + sin(angle) * hourCircleRadius;
+    var x = cx + cos(angle) * hourCirclePos;
+    var y = cy + sin(angle) * hourCirclePos;
+    strokeWeight(3);
+    stroke(80);
     ellipse(x, y, hourDiameter, hourDiameter);
+    for (var b=0; b < 360; b+= 360/60){
+      minCirclePos = hourDiameter / 2;
+      var subangle = radians(b);
+      var subx = x + cos(subangle) * minCirclePos;
+      var suby = y + sin(subangle) * minCirclePos;
+      strokeWeight(.5);
+      ellipse(subx, suby, minDiameter, minDiameter);
+    }
   }
   endShape();
 }
