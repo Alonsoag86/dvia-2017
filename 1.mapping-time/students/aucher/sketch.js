ba9139d1566230f58ad599dac8a4d8a6b118dcc3
serrau <aucher.serr@gmail.com>
2017-09-12 23:48:02 -0400

    clean code and smooth colors

--- 1.mapping-time/students/aucher/sketch.js
+++ 1.mapping-time/students/aucher/sketch.js
@@ -30,9 +30,8 @@ function setup() {
   day = color('rgb(238, 215, 135)');
   sunrise = color('rgb(224, 123,	129)');
   sunset = color('rgb(229, 154,	131)');
-  sunriseTime = 5;
-  noon = 12;
-  sunsetTime = 19;
+  sunriseTime = 6;
+  sunsetTime = 18;
 
   var radius = min(width, height) / 2; // this is the maximum possible radius
   clockDiameter = radius * 1.7; // make slightly smaller than maximum allowed
@@ -55,27 +54,9 @@ function draw() {
 
   // Angles for sin() and cos() start at 3 o'clock;
   // subtract HALF_PI to make them start at the top
-  var s = map(second(), 0, 60, 0, TWO_PI) - HALF_PI;
+  // var s = map(second(), 0, 60, 0, TWO_PI) - HALF_PI;
   var m = map(minute(), 0, 60, 0, TWO_PI) - HALF_PI;
   var h = map(hour(), 0, 24, 0, TWO_PI) - HALF_PI;
-
-  var sunriseAngle = map(sunriseTime, 0, 24, 0, TWO_PI) - HALF_PI;
-  var noonAngle = map(noon, 0, 24, 0, TWO_PI) - HALF_PI;
-  var sunsetAngle = map(sunsetTime, 0, 24, 0, TWO_PI) - HALF_PI;
-
-  // var m = map(minute() + norm(second(), 0, 60), 0, 60, 0, TWO_PI) - HALF_PI;
-  // var h = map(hour() + norm(minute(), 0, 60), 0, 24, 0, TWO_PI) - HALF_PI;
-
-  // for debugging
-  // fill(color('white'));
-  // textAlign('center');
-  // text('s: ' + s.toFixed(1) + ' m: ' + m.toFixed(1) + ' h ' + h.toFixed(1), cx, cy - 15);
-  // text('s: ' + second() + ' m: ' + minute() + ' h ' + hour(), cx, cy);
-
-  // Draw the hands of the clock
-  stroke(255);
-  strokeWeight(1);
-  // line(cx, cy, cx + cos(s) * hourCirclePos, cy + sin(s) * hourCirclePos);
 
   // Draw the minute ticks
     beginShape(POINTS);
@@ -89,15 +70,14 @@ function draw() {
     fill(getHourFill(h, angle, circleNumber)); // fill with color if hour has past
     ellipse(x, y, hourDiameter, hourDiameter);
 
-    fill(color('white')); // for debugging
-    text(circleNumber, x, y);
+    // fill(color('white')); // for debugging
+    // text(circleNumber, x, y);
     for (var b=0; b < 360; b+= 360/60){
       var subangle = radians(b) - HALF_PI;
       var subx = x + cos(subangle) * minCirclePos;
       var suby = y + sin(subangle) * minCirclePos;
-      // strokeWeight(.5);
       noStroke();
-      fill(getMinFill(h, m, angle, subangle, circleNumber));// fill(calculateColor(b)[1]);
+      fill(getMinFill(h, m, angle, subangle, circleNumber));
       ellipse(subx, suby, minDiameter, minDiameter);
     }
     circleNumber+=1;
@@ -113,11 +93,10 @@ function getHourFill(h, angle, circleNumber){
 
 function getMinFill(h, m, angle, subangle, circleNumber){
   var c = 0;
-  if (angle < h) {
+  if (angle < h)
     c = calculateColor(circleNumber);
-  } else if (angle == h && subangle < m) {
+   else if (angle == h && subangle < m)
     c = calculateColor(circleNumber);
-  }
   return c;
 }
 
@@ -125,38 +104,40 @@ function calculateColor(circleNumber){
   var startColor = night;
   var endColor = day;
   var colorScale = 0;
+  var gradientPadding = 2; // hours for the color transition
 
-  if (circleNumber < (sunriseTime - 2) || circleNumber > (sunsetTime + 1)){// Night
+
+  if (circleNumber > (sunsetTime + gradientPadding || circleNumber < (sunriseTime - gradientPadding))){// Night
     startColor = night;
     endColor = night;
     colorScale = 0; //to start color change at top
-  } else if (circleNumber >= (sunriseTime - 2) && circleNumber < (sunriseTime)) { // Night>Sunrise
+
+  } else if (circleNumber >= (sunriseTime - gradientPadding) && circleNumber < (sunriseTime)) { // Night>Sunrise
     startColor = night;
     endColor = sunrise;
-    colorScale = map ((circleNumber), sunriseTime - 2, sunriseTime, 0, 1);
-  } else if (circleNumber >= (sunriseTime) && circleNumber <= (noon - 2)) { // Sunrise>Noon
+    colorScale = map ((circleNumber), sunriseTime - gradientPadding, sunriseTime, 0, 1);
+
+  } else if (circleNumber >= (sunriseTime) && circleNumber < (sunriseTime + gradientPadding)) { // Sunrise>Noon
     startColor = sunrise;
     endColor = day;
-    colorScale = map ((circleNumber), sunriseTime + 1, noon - 2, 0, 1);
-  } else if (circleNumber > (noon - 2) && circleNumber <= (sunsetTime - 1)) { // Noon
+    colorScale = map ((circleNumber), sunriseTime, sunriseTime + gradientPadding, 0, 1);
+
+  } else if (circleNumber >= sunriseTime + gradientPadding && circleNumber < (sunsetTime - gradientPadding)) { // Noon
     startColor = day;
     endColor = day;
     colorScale = 0;
-  } else if (circleNumber > (sunsetTime - 1) && circleNumber <= (sunsetTime + 1)) { // Noon>Sunset
+
+  } else if (circleNumber >= (sunsetTime - gradientPadding) && circleNumber < (sunsetTime + gradientPadding)) { // Noon>Sunset
     startColor = day;
     endColor = sunset;
-    colorScale = map ((circleNumber), sunsetTime - 1, sunsetTime + 1, 0, 1);
-  } else if (circleNumber > (sunsetTime + 1)) { // Sunset>Night
+    colorScale = map ((circleNumber), sunsetTime - gradientPadding, sunsetTime + gradientPadding, 0, 1);
+
+  } else if (circleNumber >= (sunsetTime)) { // Sunset>Night
     startColor = sunset;
     endColor = night;
-    colorScale = map ((circleNumber), sunsetTime + 1, 24, 0, 1);
+    colorScale = map ((circleNumber), sunsetTime + 1, 21, 0, 1);
   }
 
   hourColor = lerpColor(startColor, endColor, colorScale);
   return hourColor;
 }
-
-
-
-// TODO add moonshape in the middle based on state of moon
-// TODO calculate logic for what fill to use
