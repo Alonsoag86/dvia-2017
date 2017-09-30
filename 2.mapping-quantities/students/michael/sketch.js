cc961907942d633c0264b39b85937814b7e8f9cc
wolfm2@newschool.edu <wolfm2@newschool.edu>
2017-09-29 22:59:35 -0400

    added multibar functionality

--- 2.mapping-quantities/students/michael/sketch.js
+++ 2.mapping-quantities/students/michael/sketch.js
@@ -8,13 +8,26 @@ function preload() {
   // table = loadTable("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.csv", "csv", "header");
 }
 
-barWidth = 400;
+gWidth = 1000;
+height = 0;
+
+barWidth = gWidth;
 barHeight = 30;
-barSpacer = 20;
+barSpacer = 40;
+barBuf = 10;
 barX = 50;
 barY = 50;
+textHeight = barHeight / 2;
 bColors = [];
 bCurve = 5;
+grey = 200
+
+eventX = eventY = -1;
+pcentView = 100;
+
+// our data
+labels = [];
+values = [];
 
 /*
 
@@ -28,65 +41,150 @@ dict.key1 = "value1";
 dict.key2 = "value2";
 */
 
+// sum vals of array for .reduce()
 function sum(total, num) {
-    return total + num;
+    if (num.length == undefined) {  // No tag
+      return total + num;
+    } else { // Has tag
+      if (total.length == undefined)
+        return total + num[1];
+      else
+        return total[1] + num[1];
+    }
 }
 
-function drawBar(title, vals) {
+// stackexchange collide alg
+function mouseCollide (pointX, pointY, x, y, xW, yW) {
+  if (pointX >= x &&       // right of the left edge AND
+    pointX <= x + xW &&    // left of the right edge AND
+    pointY >= y &&         // below the top AND
+    pointY <= y + yW) {    // above the bottom
+      return true;
+  }
+  return false;
+};
+
+// mouse event
+// noinfo msg
+// TODO: Bounding Bar?
+// dont draw tag when too small
+// draw info when pcentView is 0;
+function drawBars(title, vals) {
   var cIdx = 0;
   
   for (var yidx=0; yidx < vals.length; yidx++) {
-  
     
-
-    var bUnit = (1.0 * barWidth) / vals[yidx].reduce(sum);  // get base fraction of pixels 
-    println(barX);
-    println(barY + (barHeight + barSpacer)*yidx);
-    println(vals[yidx]*bUnit);
+    var curY = barY + (barHeight + barSpacer)*yidx;
+    var curX = barX;
+    var bMax = 0;
     
-    curY = barY + (barHeight + barSpacer)*yidx;
-    curX = barX;
-    
-    stroke(100);
+    var bUnit = (1.0 * barWidth) / vals[yidx].reduce(sum);  // get base fraction of pixels   
+      
+    push();  // Bounding Bar
+    stroke(grey);
     strokeWeight(3.0);
     noFill();
-    rect(curX - 5, curY - 5, barWidth + 10, barHeight + 10, bCurve, bCurve, bCurve, bCurve);
+    //rect(curX - 5, curY - 5, barWidth + barBuf, barHeight + barBuf, bCurve, bCurve, bCurve, bCurve);
+    pop()
     
+    push()
     for (var xidx = 0; xidx < vals[yidx].length; xidx++, curX+=curWidth) {
       var lCurve = 0;
       var rCurve = 0;
-      
+     
       if (xidx == 0)
         lCurve = bCurve;
         
       if (xidx == vals[yidx].length - 1)
         rCurve = bCurve;
         
+      var curTag = 0;
+      
       c = bColors[xidx%bColors.length];
       stroke(c);
       fill(c);
-      var curWidth = vals[yidx][xidx]*bUnit;
+      if (vals[yidx][xidx].length == undefined) {  // No tag
+        var curVal = vals[yidx][xidx];
+      } else { // Has tag
+        var curTag = vals[yidx][xidx][0];
+        var curVal = vals[yidx][xidx][1];
+      }
+          
+      if (bMax < curVal)
+        bMax = curVal;
+      var curWidth = curVal*bUnit;
+      
+      if (mouseCollide (mouseX, mouseY, curX, curY, curWidth, barHeight)) {
+        eventX = xidx;
+        eventY = yidx;
+      }       
       rect(curX, curY, curWidth, barHeight, lCurve, rCurve, rCurve, lCurve);
+        
+      textAlign(CENTER);
+      stroke(grey);
+      fill(grey);
+      if (curTag)  // if a tag exists, print it
+        text(curTag, curX + curWidth/2, curY + ((barHeight + barBuf)/2));
+        
+      pop();
     }
+    
+    //textFont("Helvetica", textHeight); // BUG: Why is AA so smudgy?
+    textSize(textHeight);
+    textAlign(LEFT);
+    stroke(grey);
+    fill(grey);
+    if (title.length)
+      text(title[yidx] + ": " + bMax, barX, curY - 10);
   }
 }
 
+
+
+
 function setup() {
-  bColors = [color(0,0,255), color(0,255,0), color(255,0,0)];
-// var v = [["mx", 2], ["us", 3], ["at", 5]]
-var v = [[5,7,22,3,2,6,8], [5,3,2,6,8,7,22]];
-var t = ["Magnitude","Population"];
+  bColors = [color("#005548"), color("#7D3051"), color("#2F1F26"), color("#262E35"), color("#783D28"), color("#49000D"), color("#6F6473"), color("#86414F"), color("#357157"), color("#00617E"), color("#7A6333"), color("#77242A"), color("#3E3E24"), color("#7E7E36"), color("#445B1C"), color("#3F342F"), color("#783D28"), color("#7A6333")];
 
-
+  gWidth = windowWidth; // set global width
   createCanvas(windowWidth, windowHeight);
+  
+  values = [[5,7,22,2,6,8,22,5,7,2,6,8], [5,3,2,6,8,22,5,7,22,2,6,8], [["mx", 2], ["us", 3], ["at", 5], ["mx", 2], ["us", 3], ["at", 5], ["mx", 2], ["us", 3], ["at", 5], ["mx", 2], ["us", 3], ["at", 5]]];
+  labels = ["Magnitude", "Population", "Stations"];
+}
 
-drawBar(t, v);
+function update() {
+  barWidth = gWidth - 200;
+  
+  values = [[5,7,22,2,6,8,22,5,7,2,6,8], [5,3,2,6,8,22,5,7,22,2,6,8], [["mx", 2], ["us", 3], ["at", 5], ["mx", 2], ["us", 3], ["at", 5], ["mx", 2], ["us", 3], ["at", 5], ["mx", 2], ["us", 3], ["at", 5]]];
+  labels = ["Magnitude", "Population", "Stations"];
+
+  println(eventX + " " + eventY + " " + pcentView);
+    
+  // if event is active, change the percent of other bars we view
+  if (eventY != -1) {
+    pcentView-=4;
+    eventY = -1 // disable the event
+  } else {
+    pcentView+=4;
+  }
+  
+  if (pcentView < 0) pcentView = 0;
+  if (pcentView > 100) pcentView = 100;
+  
+  for (var yidx = 0; yidx < values.length; yidx++)
+    for (var xidx = 0; xidx < values[yidx].length; xidx++)
+      if (xidx != eventX && yidx != eventY  && yidx != 2) 
+        values[yidx][xidx] = map(pcentView, 0, 100, 0, values[yidx][xidx]); 
 }
 
 function draw() {
+  update();
 
+  background(0);
+  drawBars(labels, values);
 }
 
 function windowResized() {
+  gWidth = windowWidth; // set global width
   resizeCanvas(windowWidth, windowHeight);
 }
