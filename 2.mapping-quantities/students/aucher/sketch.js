924d06a352d8f553ec1e25cd025a64c5745fb3ad
serrau <aucher.serr@gmail.com>
2017-09-30 23:22:14 -0400

    add techtonic plate map outline

--- 2.mapping-quantities/students/aucher/sketch.js
+++ 2.mapping-quantities/students/aucher/sketch.js
@@ -1,31 +1,46 @@
 // define a global variable to hold our USGS data
-var table
-// var ang = 0;
-var trialCircle;
-var opacity;
 
-// function preload() {
-//   // load data from either a local copy of one of the USGS CSVs or directly:
-//   table = loadTable("assets/significant_month.csv", "csv", "header");
-//   // or (while you're designing) from the feed itself:
-//   // table = loadTable("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.csv", "csv", "header");
-// }
+
+// sources: https://github.com/fraxen/tectonicplates
+
+//mapbox://styles/aucher/cj87xw3fi3z8a2qpbdhsqfcw8
+var table,
+  mapImg,
+  trialCircle,
+  opacity;
+
+var canvasW = 1024,
+  canvasH = 512;
+
+
+
+function preload() {
+  var accessToken = 'pk.eyJ1IjoiYXVjaGVyIiwiYSI6ImNqODd4NnBndzFjZDQyd3FocnM4Njc2NWQifQ.dql4s6oWRANbYGt44i6n9A'
+  var mapboxUrl = 'https://api.mapbox.com/styles/v1/aucher/cj87xw3fi3z8a2qpbdhsqfcw8/static/25.613590,-0.000000,0.78,0.00,0.00/' + canvasW + 'x' + canvasH + '?access_token=' + accessToken
+  println('url: ' + mapboxUrl);
+  // load data from either a local copy of one of the USGS CSVs or directly:
+  // table = loadTable("assets/significant_month.csv", "csv", "header");
+  mapImg = loadImage(mapboxUrl);
+  // or (while you're designing) from the feed itself:
+  // table = loadTable("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.csv", "csv", "header");
+}
 
 function setup() {
-  println(table)
-  createCanvas(window.innerWidth, window.innerHeight);
+  // println(table)
+  createCanvas(canvasW, canvasH);
   trialCircle = new circleBurst( width/2, height/2, 7);
   frameRate(5);
+  // globe = createShape(SPHERE, 200);
 }
 
 function draw() {
-  println(trialCircle);
+  image(mapImg, 0, 0);
   trialCircle.display();
 }
 
-function circleBurst(xPos, yPos, startDiam){
+function circleBurst(xPos, yPos, startDiam, color){
   // need an array of circles
-  var numCircles = 7;
+  var numCircles = 5;
   var stepDiameter = 10;
   var diameters = [];
 
@@ -40,11 +55,11 @@ function circleBurst(xPos, yPos, startDiam){
 
   // pulsating display function
   this.display = function(){
-    background(255);
+    // background(255);
 
     for (var c in diameters){
       this.diameters[c] = (this.diameters[c] >= this.maxDiam) ? this.startDiam : this.diameters[c] + 1
-        opacity = map(this.diameters[c], this.startDiam, this.maxDiam, 255, 0);
+      opacity = map(this.diameters[c], this.startDiam, this.maxDiam, 255, 0);
 
       // inner most circle
       fill(0);
@@ -52,7 +67,7 @@ function circleBurst(xPos, yPos, startDiam){
 
       // concentric circles
       noFill();
-      stroke(0, opacity);
+      stroke(253, 41, 252	, opacity);
       ellipse(this.x, this.y, this.diameters[c], this.diameters[c]);
     }
   };
