566d077ebea1aaed292e338e397cbbb21d5b11a5
Patrick Serr <serrp830@newschool.edu>
2017-10-04 16:22:17 -0400

    Exercise 2 Mapping Quantities -- first commit
    Versions 1, 2, 3 --> successive amounts of data and formatting
    V3 is most current, showing all earthquakes over course of day.

--- 2.mapping-quantities/students/patrick/V3 - past day revised/sketch.js
+++ 2.mapping-quantities/students/patrick/V3 - past day revised/sketch.js
@@ -8,11 +8,12 @@ var table;
 
 
 function preload() {
-  table = loadTable("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.csv", "csv", "header");
+  table = loadTable("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.csv", "csv", "header");
+  myFont = loadFont('assets/Apercu-Mono.otf');
 }
 
 function setup() {
-  createCanvas(1200, 4800);
+  createCanvas(1400, 10000);
   textSize(10);
 
   println(table);
@@ -23,11 +24,16 @@ function setup() {
   // }
 
   // yPos = yPos+5;
-
+  textFont(myFont);
   textStyle(BOLD);
   textSize(30);
-  fill(100,100,255)
-  text("Earthquakes within past month", 100, 50);
+  fill(100,100,255);
+  var d = day();
+  var m = month();
+  var yr = year();
+  text(("Earthquakes " + m + "." + d + "." + yr ), 100, 50);
+  textSize(16);
+  text("Based on live data courtesy of USGS. Updated every 15 minutes.", 100, 75);
 
   textStyle(NORMAL);
   colorMode(RGB);
@@ -40,20 +46,20 @@ function setup() {
   // text(table.getString(1, 13), 200, 200);
 
   for (var r = 1; r < table.getRowCount(); r++) {
-  var rowAlpha = table.getNum(r, 4)*30;
+  var rowAlpha = table.getNum(r, 4)*40;
   // println(rowAlpha);
   fill(0,0,0,rowAlpha);
-  textSize(rowAlpha/8);
-  text(table.getString(r, 13), 100, 100 + r*40);}
+  textSize(rowAlpha/5);
+  text(table.getString(r, 13), 250, 150 + r*40);}
 
 
   for (var r = 1; r < table.getRowCount(); r++) {
-  var rowAlpha = table.getNum(r, 4)*30;
+  var rowAlpha = table.getNum(r, 4)*40;
   // println(rowAlpha);
   fill(0,0,0,rowAlpha);
-  textSize(rowAlpha/8);
+  textSize(rowAlpha/5);
   // fill(0,0,0,.2);
-  text(table.getString(r, 4), 800, 100 + r*40);}
+  text(table.getString(r, 4), 100, 150 + r*40);}
 
 
 
