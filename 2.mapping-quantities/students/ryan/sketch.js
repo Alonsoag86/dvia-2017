8e52967de91eb29d8d00bad3c7d06d3be3f72ed4
ryanabest <ryanabest@gmail.com>
2017-10-04 12:19:20 -0400

    adjust scale of mag size

--- 2.mapping-quantities/students/ryan/sketch.js
+++ 2.mapping-quantities/students/ryan/sketch.js
@@ -51,25 +51,27 @@ function draw() {
     getmaxdepth();
   }
 
+
   var maxy = hght - ymargin - (zeroy/2);
   var miny = ymargin + (zeroy/2);
   var ydiff = (maxy-miny)/(maxd-mind);
-  var magscale = xdiff/12;
+  var magscale = xdiff/7;
 
 
   sortedtime = sort(table.getColumn("time"));
-  console.log(sortedtime);
+  // console.log(sortedtime);
   for (t=0;t<sortedtime.length;t++) {
     for (r=0;r<rowcount;r++) {
       if (sortedtime[t] === table.get(r,"time")) {
         var x = xmargin+zerox+(xdiff*(t));
         var y = ymargin+zeroy+(table.getNum(r,"depth")*ydiff);
-        var diam = (table.getNum(r,"mag")+2)*magscale;
+        var diam = (table.getNum(r,"mag")-2)*magscale;
         var rad = diam/2;
+        var rand = Math.pow(1.5,table.getNum(r,"mag"));
 
         line(x,ymargin+zeroy,x,y);
         if (mouseX <= x+rad && mouseX >= x-rad && mouseY <= y+rad && mouseY >= y-rad) {
-          ellipse(x+random(-table.getNum(r,"mag"),table.getNum(r,"mag")),y+random(-table.getNum(r,"mag"),table.getNum(r,"mag")),diam);
+          ellipse(x+random(-rand,rand),y+random(-rand,rand),diam);
           strokeWeight(0);
           textStyle(BOLD);
           text(table.getString(r,"time"),wdth/2,ymargin/3);
@@ -97,20 +99,20 @@ function draw() {
   translate(-(xmargin+(zerox/2)),-(ymargin+zeroy));
 
   textAlign(CENTER);
-  text("Magnitude",wdth-1.5*xdiff,hght-ymargin-zeroy);
+  text("Magnitude",wdth-((xmargin+zerox)/2),hght-ymargin-zeroy);
 
   if (mouseX <= wdth-2*xdiff+(xdiff/2) && mouseX >= wdth-2*xdiff-(xdiff/2) && mouseY <= hght-(xdiff/2)-5+(xdiff/24) && mouseY >= hght-(xdiff/2)-5-(xdiff/24)) {
-    ellipse(wdth-2*xdiff+random(-1,1),hght-(xdiff/2)-5+random(-1,1),xdiff/12);
+    ellipse(wdth-2*xdiff+random(-Math.pow(1.5,3),Math.pow(1.5,3)),hght-(xdiff/2)-5+random(-Math.pow(1.5,3),Math.pow(1.5,3)),magscale);
   } else {
-    ellipse(wdth-2*xdiff,hght-(xdiff/2)-5,xdiff/12);
+    ellipse(wdth-2*xdiff,hght-(xdiff/2)-5,magscale);
   };
+  text("3",wdth-2*xdiff,hght-(xdiff/2)+15 );
 
-  text("-1",wdth-2*xdiff,hght-(xdiff/2)+15 );
   if (mouseX <= wdth-xdiff+(xdiff/2) && mouseX >= wdth-xdiff-(xdiff/2) && mouseY <= hght-5 && mouseY >= hght-5-xdiff) {
-    ellipse(wdth-xdiff+random(-12,12),hght-(xdiff/2)-5+random(-12,12),xdiff);
+    ellipse(wdth-xdiff+random(-Math.pow(1.5,9),Math.pow(1.5,9)),hght-(xdiff/2)-5+random(-Math.pow(1.5,9),Math.pow(1.5,9)),xdiff);
   } else {
     ellipse(wdth-xdiff,hght-(xdiff/2)-5,xdiff);
   }
-  text("10",wdth-xdiff,hght-(xdiff/2)-5)
+  text("9",wdth-xdiff,hght-(xdiff/2)-5)
 
 };
