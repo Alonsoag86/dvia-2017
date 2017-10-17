e364201c5af536ed0cc6c241fba3a7a660e8edd0
3milychu <3milychu@gmail.com>
2017-10-17 18:23:44 -0400

    wip

--- 3.textual-and-qualitative-data/students/emily/render.js
+++ 3.textual-and-qualitative-data/students/emily/render.js
@@ -8,13 +8,29 @@ var tmplSource = fs.readFileSync('template.html', 'utf-8'),
     data = JSON.parse(fs.readFileSync('assets/shows.json', 'utf-8'));
 
 var byArtist = _.groupBy(data, 'artist');
-    byVenue = _.groupBy(data, 'venue'),
+    byVenue = _.groupBy(data, 'venue');
+    listVenue = _.uniq(_.map(data, 'venue')).sort();
+	// showCount =  _.countBy(data, 'venue');
     names = _.uniq(_.map(data, 'artist')).sort();
+    duration = _.uniq(_.map(data, 'duration')).sort();
 
 var shows = []
 names.forEach(function(name){
   shows.push(byArtist[name])
-})
+});
 
-var markup = template({names:names, shows:shows})
-fs.writeFileSync('site/index.html', markup)
+var venues = []
+listVenue.forEach(function(venue){
+  venues.push(byVenue[venue]);
+  showCount = _.countBy(venues, 'venue');
+});
+
+var totalShowCount =  _.countBy(venues, 'venue');
+
+// var duration = []
+// duration.forEach(function(time){
+// 	duration.push(duration[time])
+// });
+
+var markup = template({names:names, shows:shows, venues:venues, showCount:showCount, totalShowCount})
+fs.writeFileSync('site/index.html', markup);
