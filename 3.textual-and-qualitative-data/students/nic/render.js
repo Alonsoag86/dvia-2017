6f9bb8c44475f9b9be32943f6d30916796ddfdda
Nic Stark <hello@nicstark.com>
2017-10-18 12:48:03 -0400

    backup

--- 3.textual-and-qualitative-data/students/nic/render.js
+++ 3.textual-and-qualitative-data/students/nic/render.js
@@ -9,12 +9,19 @@ var tmplSource = fs.readFileSync('template.html', 'utf-8'),
 
 var byArtist = _.groupBy(data, 'artist');
     byVenue = _.groupBy(data, 'venue'),
-    names = _.uniq(_.map(data, 'venue')).sort();
+    names = _.uniq(_.map(data, 'artist')).sort();
+    location = _.uniq(_.map(data, 'venue')).sort();
+    duration = _.uniq(_.map(data, 'duration')).sort();
 
 var shows = []
 names.forEach(function(name){
   shows.push(byVenue[name])
 })
 
-var markup = template({names:names, shows:shows})
+var venues = []
+location.forEach(function(venue){
+	venues.push(byVenue[venue])
+})
+
+var markup = template({names:names, shows:shows, venues:venues, location:location})
 fs.writeFileSync('site/index.html', markup)
