b7639b6b3dcf975ec1b1bb6872144f9e3c40de0e
Nic Stark <hello@nicstark.com>
2017-10-18 17:22:26 -0400

    got something working

--- 3.textual-and-qualitative-data/students/nic/render.js
+++ 3.textual-and-qualitative-data/students/nic/render.js
@@ -7,11 +7,20 @@ var tmplSource = fs.readFileSync('template.html', 'utf-8'),
     template = handlebars.compile(tmplSource),
     data = JSON.parse(fs.readFileSync('assets/shows.json', 'utf-8'));
 
+
+
 var byArtist = _.groupBy(data, 'artist');
     byVenue = _.groupBy(data, 'venue'),
     names = _.uniq(_.map(data, 'artist')).sort();
     location = _.uniq(_.map(data, 'venue')).sort();
-    duration = _.uniq(_.map(data, 'duration')).sort();
+
+
+var day0 = _.filter(data, function(d) {return d.time.day == 0; }),
+    day1 = _.filter(data, function(d) {return d.time.day == 1; }),
+    day2 = _.filter(data, function(d) {return d.time.day == 2; }), 
+    day3 = _.filter(data, function(d) {return d.time.day == 3; });
+
+
 
 var shows = []
 names.forEach(function(name){
@@ -20,8 +29,8 @@ names.forEach(function(name){
 
 var venues = []
 location.forEach(function(venue){
-	venues.push(byVenue[venue])
+	venues.push(names[names])
 })
 
-var markup = template({names:names, shows:shows, venues:venues, location:location})
+var markup = template({names:names, shows:shows, venues:venues, location:location, day0:day0, day1:day1, day2:day2, day3:day3})
 fs.writeFileSync('site/index.html', markup)
