5e562faf3e7ae8c7fc22ad38d3189db7257c160e
Christian Swinehart <drafting@samizdat.cc>
2017-10-24 11:28:36 -0400

    now calculating the total duration of shows per-venue
    - also including each venue’s shows as the .shows attribute of the
    venue object

--- 3.textual-and-qualitative-data/students/emily/render.js
+++ 3.textual-and-qualitative-data/students/emily/render.js
@@ -20,7 +20,7 @@ var showsCount = _.countBy(data,'venue')
 var totalCount = _.sum(_.values(showsCount))
 // console.log('total number of shows at all venues:', totalCount)
 var totalShowCount =  _.countBy(venues, 'venue');
-console.log(totalShowCount);
+// console.log(totalShowCount);
 // console.log('total number of venues:', _.keys(showsCount).length)
 
 var shows = []
@@ -35,19 +35,17 @@ var venues = []
     });;
 
 var venueObjects = [];
-    for (var venueName in venueShows){ // iterate through the keys of venueShows
-    var totalDuration = _.sum(_.values('time.duration')); //Attempt 1: sum all the time.duration values for a venueName
-    var venueObj = { venue:venueName, duration:totalDuration[venueName], //Attempt 1: return the sum for each venue and store in array
-    numShows:venueShows[venueName].length };
+for (var venueName in venueShows){ // iterate through the keys of venueShows
+    var totalDuration = _.sum(_.map(venueShows[venueName], 'time.duration')); //Attempt 1: sum all the time.duration values for a venueName
+    var venueObj = {
+        venue:venueName,
+        duration:totalDuration, //Attempt 1: return the sum for each venue and store in array
+        numShows:venueShows[venueName].length,
+        shows:venueShows[venueName]
+    };
     venueObjects.push(venueObj)
-    };
-    console.log(venueObj);
-
-var durationByVenue = []; //Atempt 2: Create an array that goes through each venue and sums all the values for duration for each venue
-    for (i=0; i<venues.length; i++){
-    var totalDuration = _.sum(_.values(venues, 'duration')); 
-    console.log(durationByVenue);
-    };
+};
+console.log(venueObjects);
 
 // sort our list of venue objects by the number of shows per-venue
 var countedVenues = _.sortBy(venueObjects, 'numShows');
@@ -56,6 +54,6 @@ var countedVenues = _.sortBy(venueObjects, 'numShows');
 // sort the original list of venue objects by venue name
 var alphabetizedVenues = _.sortBy(venueObjects, 'venue');
 
-var markup = template({names:names, shows:shows, venues:venues, showCount:showCount, 
+var markup = template({names:names, shows:shows, venues:venues, showCount:showCount,
               totalShowCount:totalShowCount, totalCount:totalCount})
 fs.writeFileSync('site/index.html', markup);
