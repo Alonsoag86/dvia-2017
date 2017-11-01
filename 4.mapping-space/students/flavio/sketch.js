6246d33a65d165707245e2b83faabb42d86439ed
Flavio Sant Anna <flaviopessoa@Flavios-MBP.wireless.newschool.edu>
2017-11-01 18:58:23 -0400

    Finish for now

--- 4.mapping-space/students/flavio/sketch.js
+++ 4.mapping-space/students/flavio/sketch.js
@@ -9,6 +9,8 @@ var latitudes, longitudes;
 var magnitudeMin, magnitudeMax;
 var depthMin, depthMax;
 
+var type;
+
 // the dots we'll be adding to the map
 var circles = [];
 
@@ -17,6 +19,8 @@ var table;
 
 // my leaflet.js map
 var mymap;
+
+var ltlg;
 
 function preload() {
     // load the CSV data into our `table` variable and clip out the header row
@@ -30,10 +34,10 @@ function setup() {
     If you want to draw some diagrams to complement the map view, set up your canvas
     size, color, etc. here
     */
-    createCanvas(100, 100);
-    background(200);
-    textSize(64);
-    text("quack", 18, 72);
+
+   // Move the canvas so it's inside our <div id="sketch-holder">.
+    var canvas = createCanvas(1, 1);
+    canvas.parent('dashdata');
 
     /*
     LEAFLET CODE
@@ -47,27 +51,50 @@ function setup() {
     mymap = L.map('quake-map').setView([51.505, -0.09], 3);
 
     // load a set of map tiles (you shouldn't need to touch this)
-    L.tileLayer('https://api.mapbox.com/styles/v1/flaviopessoa/cj9g2gkbh8l142rpc50npax8y/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZmxhdmlvcGVzc29hIiwiYSI6IlE1REw1dnMifQ.qVX5tyZ1KQ0os20vZ8pR7w', {
+    L.tileLayer('https://api.mapbox.com/styles/v1/flaviopessoa/cj9g2e2vu8l5p2rmq5yemwywl/tiles/256/{z}/{x}/{y}?access_token=pk.eyJ1IjoiZmxhdmlvcGVzc29hIiwiYSI6IlE1REw1dnMifQ.qVX5tyZ1KQ0os20vZ8pR7w', {
         attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
         maxZoom: 18,
         id: 'mapbox.streets',
         accessToken: 'pk.eyJ1IjoiZHZpYTIwMTciLCJhIjoiY2o5NmsxNXIxMDU3eTMxbnN4bW03M3RsZyJ9.VN5cq0zpf-oep1n1OjRSEA'
     }).addTo(mymap);
 
-
     // call our function (defined below) that populates the maps with markers based on the table contents
+    // drawBarCharts();
     drawDataPoints();
+
+
 }
 
+// function drawBarCharts(){
+//   type = table.getColumn("type");
+//
+//   var counts = {};
+//   for (var i = 0; i < type.length; i++) {
+//   counts[type[i]] = 1 + (counts[type[i]] || 0);
+//   };
+//
+//   noStroke();
+//   fill('white');
+//   for (var i=1; i < counts.length; i++){
+//     rect(i*5, 300, 5, 10);
+//   }
+// // line(x, y, x, y);
+// }
+
 function drawDataPoints(){
-    strokeWeight(5);
-    stroke(255,0,0);
 
     // get the two arrays of interest: depth and magnitude
     depths = table.getColumn("depth");
     magnitudes = table.getColumn("mag");
     latitudes = table.getColumn("latitude");
     longitudes = table.getColumn("longitude");
+    type = table.getColumn("type");
+
+    var counts = {};
+    for (var i = 0; i < type.length; i++) {
+    counts[type[i]] = 1 + (counts[type[i]] || 0);
+    };
+    console.log(counts);
 
     // get minimum and maximum values for both
     magnitudeMin = 0.0;
@@ -80,21 +107,58 @@ function drawDataPoints(){
 
     // cycle through the parallel arrays and add a dot for each event
     for(var i=0; i<depths.length; i++){
+
         // create a new dot
-        var circle = L.circle([latitudes[i], longitudes[i]], {
-            color: 'red',      // the dot stroke color
-            fillColor: '#f03', // the dot fill color
-            fillOpacity: 0.25,  // use some transparency so we can see overlaps
-            radius: magnitudes[i] * 40000
+        var circle = L.circleMarker([latitudes[i], longitudes[i]], {
+
+            fillColor: typeColor(i), // the dot fill color
+            fillOpacity: 0.4,  // use some transparency so we can see overlaps
+            radius: 10,
+            stroke: stColorProg(i),
+            color: 'grey',
+            weight: 1,
+            className: typeName(i)
         });
 
         // place it on the map
         circle.addTo(mymap);
 
         // save a reference to the circle for later
-        circles.push(circle)
+        circles.push(circle);
     }
 }
+
+console.log(circles);
+
+function opacityProg(x) {
+  if (type[x] == 'earthquake') return 0.1;
+  if (type[x] == 'explosion') return 0.5;
+  if (type[x] == 'ice quake') return 0.5;
+  if (type[x] == 'quarry blast') return 0.5;
+  if (type[x] == 'sonic boom') return 0.5;
+  if (type[x] == 'chemical explosion') return 0.5;
+}
+
+function stColorProg(x){
+  if (magnitudes[x] >= 5.5) return true; else return false};
+
+function typeColor(x){
+    if (type[x] == 'earthquake') return 'lightblue';
+    if (type[x] == 'explosion') return 'yellow';
+    if (type[x] == 'ice quake') return 'orange';
+    if (type[x] == 'quarry blast') return 'pink';
+    if (type[x] == 'sonic boom') return 'green';
+    if (type[x] == 'chemical explosion') return 'red';
+    };
+
+function typeName(x){
+    if (type[x] == 'earthquake') return 'earthquake';
+    if (type[x] == 'explosion') return 'explosion';
+    if (type[x] == 'ice quake') return 'ice quake';
+    if (type[x] == 'quarry blast') return 'quarry blast';
+    if (type[x] == 'sonic boom') return 'sonic boom';
+    if (type[x] == 'chemical explosion') return 'chemical explosion';
+    };
 
 function removeAllCircles(){
     // remove each circle from the map and empty our array of references
